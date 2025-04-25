e504e4326b90597303d875b257812abfc8ddf037
Deniz Özaltay <denizozaltay2003@hotmail.com>
2025-04-25 15:20:01 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,7 @@ export default async function Home() {
   return (
     <>
       <p>Ana Sayfa</p>
-      <Link href={"/csr-fetch"}>CSR Fetch Sayfası</Link>
-      <Card className="w-full max-w-xs overflow-hidden transition-all duration-300 hover:shadow-lg">
-        <div className="relative overflow-hidden">
-          <img src="https://mylifebox.com/img/blog/blog18/3.png" />
-        </div>
-      </Card>
+      <Link href={"/products"}>Products</Link>
     </>
   );
 }
